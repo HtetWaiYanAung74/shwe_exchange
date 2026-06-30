9c985f98e0e02047b55de410269103e49941f812
HtetWaiYanAung74 <htetwyag75@gmail.com>
2026-07-01 00:06:33 +0700

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,6 +26,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: process.env.CI === 'true',
+    
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: process.env.BASE_URL,
 
